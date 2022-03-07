fd9364981d65ecdc1a6fd1bae6dfc466f9d0afee
Nicolas Binford <7463943+binfordn@users.noreply.github.com>
2022-03-07 14:23:45 -0800

    Update serial_driver.py
    More serial object arguments and better variable names

--- serial-device/serial_driver.py
+++ serial-device/serial_driver.py
@@ -3,31 +3,44 @@
 import serial
 
 
+# This value may need to be found through trial and error
+DEVICE_NAME = "/dev/ttyUSB0"
+
+# These values should be in the device documentation
 BAUDRATE = 9600
-DEVICE_FILE = "/dev/ttyUSB0"
-TIMEOUT = 2
+BYTESIZE = serial.EIGHTBITS
+PARITY = serial.PARITY_NONE
+STOPBITS = serial.STOPBITS_ONE
+
+# Optional timeouts in seconds
+READ_TIMEOUT = 2
+WRITE_TIMEOUT = 2
 
 
 class SerialDriver():
     def __init__(self):
-        self.baudrate = BAUDRATE
-        self.device_file = DEVICE_FILE
 
-        self.port = serial.Serial(
-            self.device_file, self.baudrate, timeout=TIMEOUT)
+        self.serial_object = serial.Serial(
+            port=DEVICE_NAME,
+            baudrate=BAUDRATE,
+            bytesize=BYTESIZE,
+            parity=PARITY,
+            stopbits=STOPBITS,
+            timeout=READ_TIMEOUT,
+            write_timeout=WRITE_TIMEOUT)
 
     def _reset_buffers(self):
         """
         Worker function
         """
-        self.port.reset_input_buffer()
-        self.port.reset_output_buffer()
+        self.serial_object.reset_input_buffer()
+        self.serial_object.reset_output_buffer()
 
     def _read_response(self):
         """
         Worker function
         """
-        output_lines = self.port.readlines()
+        output_lines = self.serial_object.readlines()
         output_string = ""
 
         for l in output_lines:
@@ -44,8 +57,8 @@ class SerialDriver():
         command = my_command
         command += SERIAL_ACK
 
-        self.port.write(command.encode())
-        self.port.flush()
+        self.serial_object.write(command.encode())
+        self.serial_object.flush()
 
     def example_driver_function(self):
         """
