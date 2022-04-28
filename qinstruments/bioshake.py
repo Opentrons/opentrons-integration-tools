641cae5bd39204215fed81e4a658a6541f1b04b6
Nicolas Binford <7463943+binfordn@users.noreply.github.com>
2022-04-28 15:18:22 -0700

    Add files via upload

---
+++ qinstruments/bioshake.py
@@ -0,0 +1,94 @@
+import serial
+
+
+class BioshakeDriver():
+    def __init__(self, my_device_name="/dev/ttyUSB0"):
+        # Default Bioshake device name is /dev/ttyUSB0
+        # If multiple Bioshakes are connected, you will need to find which
+        # device uses which device file (ttyUSB0, ttyUSB1...)
+        # (See "serial-device/get_usb_devices.py")
+        DEVICE_NAME = my_device_name
+    
+        # Specified in the QInstruments documentation
+        BAUDRATE = 9600
+        BYTESIZE = serial.EIGHTBITS
+        PARITY = serial.PARITY_NONE
+        STOPBITS = serial.STOPBITS_ONE
+
+        # Optional timeouts in seconds
+        READ_TIMEOUT = 2
+        WRITE_TIMEOUT = 2
+
+        self.serial_object = serial.Serial(
+            port=DEVICE_NAME,
+            baudrate=BAUDRATE,
+            bytesize=BYTESIZE,
+            parity=PARITY,
+            stopbits=STOPBITS,
+            timeout=READ_TIMEOUT,
+            write_timeout=WRITE_TIMEOUT)
+
+    def _reset_buffers(self):
+        """
+        Worker function
+        """
+        self.serial_object.reset_input_buffer()
+        self.serial_object.reset_output_buffer()
+
+    def _read_response(self):
+        """
+        Worker function
+        """
+        output_lines = self.serial_object.readlines()
+        output_string = ""
+
+        for l in output_lines:
+            output_string += l.decode("utf-8")
+
+        return output_string
+
+    def _send_command(self, my_command):
+        """
+        Worker function
+        """
+        SERIAL_ACK = "\r\n"
+
+        command = my_command
+        command += SERIAL_ACK
+
+        self.serial_object.write(command.encode())
+        self.serial_object.flush()
+
+    def get_info(self):
+        self._send_command("info")
+
+        info = self._read_response()
+
+        return info
+
+    def home_shaker(self):
+        self._send_command("shakeGoHome")
+
+    def set_shake(self, my_rpm, my_shake_time):
+        rpm = str(my_rpm)
+        shake_time = str(my_shake_time)
+
+        self._send_command("ssts{}".format(rpm))
+        self._send_command("shakeOnWithRuntime{}".format(shake_time))
+
+    def temp_on(self, my_temp):
+        temp = float(my_temp)*10
+        temp = int(temp)
+        temp = str(temp)
+
+        self._send_command("setTempTarget{}".format(temp))
+        self._send_command("tempOn")
+
+    def temp_off(self):
+        self._send_command("tempOff")
+
+    def unlock(self):
+        self._send_command("setElmUnlockPos")
+
+    def lock(self):
+        self._send_command("setElmLockPos")
---
+++ qinstruments/protocol_bioshake.py
@@ -0,0 +1,63 @@
+import sys
+import serial
+
+from opentrons import protocol_api
+
+# Python modules that have been uploaded to the Jupyter notebook
+# (bioshake.py, labware_modifier.py)
+sys.path.append("/var/lib/jupyter/notebooks/")
+import bioshake
+import labware_modifier
+
+
+metadata = {
+    'protocolName': 'Protocol Bioshake',
+    'author': 'Opentrons <protocols@opentrons.com>',
+    'description': 'Use QInstruments Bioshake with serial driver',
+    'apiLevel': '2.12'
+}
+
+
+def bioshake_action(ctx):
+    ctx.comment("Bioshake action function")
+
+    # See bioshake.py
+    my_bioshake = bioshake.BioshakeDriver()
+    my_bioshake.unlock()
+    ctx.delay(2)
+    my_bioshake.lock()
+    ctx.delay(2)
+
+    # Not blocking, wait for action to complete
+    my_bioshake.set_shake(1000, 5)
+    ctx.delay(6)
+
+
+def run(ctx):
+    # Load tip rack
+    tip_rack = ctx.load_labware("opentrons_96_tiprack_300ul", 5)
+
+    # Load well plate that is on Bioshake using the Bioshake's geometry
+    # (see "labware-modifier")
+    bioshake_x = -3.577
+    bioshake_y = 3.665
+    bioshake_z = 63
+    labware_with_offsets = labware_modifier.set_labware_offsets(
+        labware_name = "nest_96_wellplate_2ml_deep",
+        x = bioshake_x,
+        y = bioshae_y,
+        z = bioshake_z)
+    plate = ctx.load_labware_from_definition(labware_with_offsets, 10)
+
+    # Load pipette
+    p300 = ctx.load_instrument("p300_single_gen2", "left",
+                               tip_racks=[tip_rack])
+
+    p300.pick_up_tip()
+
+    p300.move_to(plate['A1'].top())
+
+    if not ctx.is_simulating():
+        bioshake_action(ctx)
+
+    p300.return_tip()
