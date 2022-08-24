b6bdacdba5efd4c4e58e11cad5091725448987b1
Nicolas Binford <7463943+binfordn@users.noreply.github.com>
2022-08-24 12:08:40 -0700

    Add probe switch status script

---
+++ ot2-doodads/trash-bin-switch/get_probe_switch.py
@@ -0,0 +1,18 @@
+# Returns state of trash bin probe switches
+# Have to run as Python script; not as Jupyter notebook
+
+import asyncio
+from opentrons import hardware_control
+
+
+async def main():
+
+    my_hc_api = await hardware_control.API.build_hardware_controller()
+
+    switches = await my_hc_api._backend._smoothie_driver.switch_state()
+
+    # 1 if any probe switch pressed (trash bin is in), 0 if not
+    probe_state = switches["Probe"]
+    print(probe_state)
+
+asyncio.run(main())
