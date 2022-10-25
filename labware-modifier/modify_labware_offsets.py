26654d00f481a063c5a3490ceebd44fd737798cc
Nicolas Binford <7463943+binfordn@users.noreply.github.com>
2022-10-25 14:26:34 -0700

    Major overhaul of labware offsets program

--- labware-modifier/modify_labware_offsets.py
+++ labware-modifier/modify_labware_offsets.py
@@ -1,31 +1,55 @@
-import os
+# For labware that is mounted on a custom device: create a new labware
+# definition that is a modification of an existing Opentrons definition
+
+import sys
 import json
+import os
+
+from opentrons.protocols import labware
 
 
-def set_labware_offsets(labware_name, x, y, z):
-    """
-    labware_name: either the Opentrons labware API name, or custom 
-                  labware file path
-    x, y, z: offsets in mm
-    
-    returns: labware definition dict used by "load_labware_by_definition()"
-    """
+DEVICE_NAME = "my_custom_device"
+# How much the device "offsets" the labware in xyz
+DEVICE_X = 1
+DEVICE_Y = 1
+DEVICE_Z = 50
 
-    if ".json" in labware_name:
-        labware_file = labware_name
-    else:
-        # An Opentrons labware definition is stored in the file:
-        # <ot2_labware_path> / <labware API name> / "1.json"
-        ot2_labware_path = "/usr/lib/python3.7/site-packages/opentrons_shared_data/data/labware/definitions/2/"
-        labware_file = os.path.join(ot2_labware_path, labware_name, "1.json")
 
-    with open(labware_file, "r") as f:
+# Function so this can be imported and used as a Python module
+def modify_labware(labware_name):
 
-        def_with_offsets = json.load(f)
+    # Get this labware definition as a dictionary
+    lab_dict = labware.get_labware_definition(lab_name)
 
-        # Modify the labware definition dict with your offsets
-        def_with_offsets['cornerOffsetFromSlot']['x'] = x
-        def_with_offsets['cornerOffsetFromSlot']['y'] = y
-        def_with_offsets['cornerOffsetFromSlot']['z'] = z
+    # Make modifications
+    lab_name_mod = "{}_on_{}".format(lab_name, DEVICE_NAME)
 
-        return def_with_offsets
+    lab_dict['cornerOffsetFromSlot']['x'] = DEVICE_X
+    lab_dict['cornerOffsetFromSlot']['y'] = DEVICE_Y
+    lab_dict['cornerOffsetFromSlot']['z'] = DEVICE_Z
+    lab_dict['metadata']['displayName'] = lab_name_mod
+
+    return lab_dict
+
+
+# If run standalone, creates a new JSON definition
+if __name__ == "__main__":
+
+    # Set parameters
+    if len(sys.argv) != 2:
+        print("Usage: labware_modifier_standalone <opentrons_labware_name>")
+        sys.exit()
+
+    # Opentrons labware definition name, e.g.
+    # opentrons_15_tuberack_nest_15ml_conical
+    lab_name = sys.argv[1]
+
+    lab_name_mod = "{}_on_{}".format(lab_name, DEVICE_NAME)
+    lab_dict_mod = modify_labware(labware_name=lab_name)
+
+    # Write the modified labware definition to a new JSON file
+    out_path = os.path.join(os.getcwd(), "{}.json".format(lab_name_mod))
+    with open(out_path, "w") as fw:
+        fw.write(json.dumps(lab_dict_mod))
+
+    print("Labware definition written to file: {}".format(out_path))
